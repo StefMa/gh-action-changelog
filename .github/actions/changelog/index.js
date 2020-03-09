a88e3c66a13231edbb4343182e12264883e8a1f7
StefMa <StefMaDev@outook.com>
2020-03-09 20:03:58 +0100

    Init commit
    Add tester

---
+++ .github/actions/changelog/index.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const nameToGreet = core.getInput('trigger');
+  if (trigger == "always") {
+    generateChangelog()
+  }
+  if (trigger == "none") {
+    // Do nothing
+    console.log("Skipped")
+    return
+  }
+  generateChangelog()
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+function generateChangelog() {
+  console.log("Generate changelog")
+  console.log(github.event.pull_request.number)
+}
