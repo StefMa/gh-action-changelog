541f60515b55e499cff9dd20a21fa295225bb02f
StefMa <StefMaDev@Outlook.com>
2020-03-09 20:55:29 +0100

    Hm

--- .github/actions/changelog/index.js
+++ .github/actions/changelog/index.js
@@ -1,8 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const shell = require('shelljs');
 
 try {
   const trigger = core.getInput('trigger');
+  const dir = core.getInput('changelogDir');
   if (trigger == "always") {
     generateChangelog()
     return
@@ -19,7 +21,14 @@ try {
 
 function generateChangelog() {
   console.log("Generate changelog")
-  console.log(process.env.GITHUB_EVENT_PATH)
+
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  shell.touch(`/tmp/${payload.number}`)
+  shell.ShellString("HelloWrold").to(`/tmp/${payload.number}`)
+
+  var str = shell.head(`/tmp/${payload.number}`)
+
+  console.log(str)
 }
