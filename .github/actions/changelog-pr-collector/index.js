eb3de8593dd3cacaebb6c993545293a27b257e03
StefMa <StefMaDev@Outlook.com>
2020-03-10 16:05:19 +0100

    We

--- .github/actions/changelog-pr-collector/index.js
+++ .github/actions/changelog-pr-collector/index.js
@@ -1,11 +1,34 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const shell = require('shelljs')
+import { v4 as uuidv4 } from 'uuid';
 
 try {
   const path = core.getInput('path')
-  const content = core.getInput('content')
   console.log(`Path: ${path}`)
-  console.log(`Content: ${content}`)
+
+  var pathes = path.split('\n')
+
+  var pathesContent = getConentFromPathes(pathes)
+  var pathChangelog = createChangelog(pathContent)
+
+  var dirPath = `/tmp/gh_action_changelog/${uuidv4()}`
+  var filePath = `${dirPath}/changelog`
+  shell.mkdir('-p', dirPath)
+  shell.ShellString(pathChangelog).to(filePath)
+
+  core.setOutput('path', filePath)
+  core.setOutput('content', pathChangelog)
 } catch (error) {
   core.setFailed(error.message);
+}
+
+function getConentFromPathes(pathes) {
+  return pathes.map(path => {
+    shell.head(path)
+  })
+}
+
+function createChangelog(content) {
+  return content.join('\n')
 }
