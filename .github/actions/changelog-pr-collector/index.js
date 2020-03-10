ae74620042bb7269550d29b1b1e0fabab0dcfcec
StefMa <StefMaDev@Outlook.com>
2020-03-10 13:29:09 +0100

    a

--- .github/actions/changelog-pr-collector/index.js
+++ .github/actions/changelog-pr-collector/index.js
@@ -4,8 +4,8 @@ const github = require('@actions/github')
 try {
   const path = core.getInput('path')
   const content = core.getInput('content')
-  console.log(`Patth: ${path}`)
-  console.log(`Patth: ${content}`)
+  console.log(`Path: ${path}`)
+  console.log(`Content: ${content}`)
 } catch (error) {
   core.setFailed(error.message);
 }
